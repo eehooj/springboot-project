f21ef065f67a99a0ffb95aea3691b7ece88a14c7
toriSSi <jooh21020@gmail.com>
2020-06-22 20:19:24 +0900

    피드백!!

--- src/main/java/com/github/torissi/resttemplate/controller/ReCaptChaController.java
+++ src/main/java/com/github/torissi/resttemplate/controller/ReCaptChaController.java
@@ -25,12 +25,12 @@ public class ReCaptChaController {
         } catch (Exception exception) {
             resultResponse.setMessage(exception.getMessage());
             resultResponse.setCode(400);
-            return new ResponseEntity<ResultResponse>(resultResponse, HttpStatus.OK);
+            return ResponseEntity.ok(resultResponse);
         }
 
         resultResponse.setMessage("로봇이 아닙니다.");
         resultResponse.setCode(200);
-        return new ResponseEntity<>(resultResponse, HttpStatus.OK);
+        return ResponseEntity.ok(resultResponse);
     }
 }
 
@@ -40,4 +40,26 @@ public class ReCaptChaController {
 * ResponseEntity<ResultResponse>(resultResponse, HttpStatus.OK); 라고 했는데 사실 HttpStatus.BAD_REQUEST 넣고 싶음
 * 그러나 넣으면 진짜 400 에러 발생..ㅎ
 * 에러 처리 너무 어려움,,,,
+* */
+
+/*피드백 내용
+* 1. ResponseEntity
+*   - 생성자 방식 : new 할 때 마다 heap메모리 자리를 차지함 => 메모리 효율이 비교적 좋지 않음
+*   - builder : static 처럼 한 번 메모리에 올라가면 계속해서 그 정보를 사용
+* 2. HttpStatus.BAD_REQUEST
+*   - header에 400에러가 새겨지기 때문에 스크립트단에서 에러 발생하는 것 맞음
+*   - 때문에 일단 ok로 컨트롤러 단에서 보내고 그 앞단인 interceptor 또는 filter로 파싱을 하여 원하는 값을 던지자
+*   - AOP, interceptor, filter 공부 필요
+* 3. webjar
+*   - webjar로 js를 pom.xml로 주입을 받으면 좋은 점이 버전 관리가 용이함
+*   - 만약 넣어줘야하는 라이브러리가 여러 개 인 경우 webjar를 사용하면 간단하게 주입할 수 있음 (예를 들면 bootstrap)
+* 
+* * 다음 미션 * *
+*  - 연/월/일을 string으로 받은 후 localDate로 바꾸는 util을 만듦
+*  - 그리고 그 util을 @Autowired를 통해 컨트롤러나 서비스단에 주입하기
+*  - 이 작업을 하면서 bean lifecycle을 공부하자! 
+* 
+* * 그 다음 미션 * *
+*  - properties로 설정한 것들 java config로 바꾸기
+*  - springboot 공식 문서 중에서 config 설정하는 거 있음 => 찾아보기
 * */
--- src/test/java/com/github/torissi/resttemplate/service/impl/CaptchaServiceImplTest.java
+++ src/test/java/com/github/torissi/resttemplate/service/impl/CaptchaServiceImplTest.java
@@ -2,7 +2,7 @@ package com.github.torissi.resttemplate.service.impl;
 
 import com.github.torissi.resttemplate.exception.ReCaptchaException;
 import com.github.torissi.resttemplate.model.response.ReCaptchaResponse;
-import com.github.torissi.resttemplate.service.CaptchaService;
+import com.github.torissi.resttemplate.service.ReCaptchaService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +17,14 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @ExtendWith(SpringExtension.class)
-@RestClientTest(CaptchaService.class) //REST Client test
+@RestClientTest(ReCaptchaService.class) //REST Client test
 class CaptchaServiceImplTest {
 
-    private String token = "03AGdBq24sy3gqEoKowVCK3Ul-gHR6S7nX8Sld6g-pLXLCgsn_0PKVbZ1FokOYLOvIcq9oD4TA8TkzEuvWN0uzK2dMwbnGs0andLsXmzDYPVac3sqDHD6aKWV0NEqhT0bM0yUp96qT_Avj1kD57M9BP97iTJXKkWEA3N7inFdpTnrMvRN2E-wWSsM4gb_RFBJaF6ktAnQ_a5w_OoEn-caARuzgiwOR8RwyB3qSp-8k-KaJkgqVmi6V8TLFpUNKvNBkw9f6bYw-TGy-rTnDfre4-7jVRbK4QijpBeyLrBSQr7da_6q-p-HDnc2vpHyyzX6MLU2Cgdu2ylBhZni4-SqVD8smHPEXRVwtHG0AznXHXlOYwpfn1qltUObFAxN9tePfDqs3AYzY6SOT";
+/*    private String token = "03AGdBq24sy3gqEoKowVCK3Ul-gHR6S7nX8Sld6g-pLXLCgsn_0PKVbZ1FokOYLOvIcq9oD4TA8TkzEuvWN0uzK2dMwbnGs0andLsXmzDYPVac3sqDHD6aKWV0NEqhT0bM0yUp96qT_Avj1kD57M9BP97iTJXKkWEA3N7inFdpTnrMvRN2E-wWSsM4gb_RFBJaF6ktAnQ_a5w_OoEn-caARuzgiwOR8RwyB3qSp-8k-KaJkgqVmi6V8TLFpUNKvNBkw9f6bYw-TGy-rTnDfre4-7jVRbK4QijpBeyLrBSQr7da_6q-p-HDnc2vpHyyzX6MLU2Cgdu2ylBhZni4-SqVD8smHPEXRVwtHG0AznXHXlOYwpfn1qltUObFAxN9tePfDqs3AYzY6SOT";
     //토큰을 매 번 새로운 걸 써야하는데 어떻게 해야할 지 모르겠음
 
     @Autowired
-    CaptchaService captchaService;
+    ReCaptchaService reCaptchaService;
 
     @Test
     public void getScore() throws ReCaptchaException {
@@ -38,8 +38,8 @@ class CaptchaServiceImplTest {
                             .andRespond(withSuccess(expectResult, MediaType.APPLICATION_JSON));
 
 
-        Boolean reCaptchaResponse = captchaService.reCaptchaDecision(token);
+        Boolean reCaptchaResponse = reCaptchaService.reCaptchaDecision(token);
 
-        assertThat(reCaptchaResponse).isEqualTo("true");
-    }
+        assertThat(reCaptchaResponse).isEqualTo("false");
+    }*/
 }
